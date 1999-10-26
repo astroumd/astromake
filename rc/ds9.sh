009650d7e66ab867d72461cf1c0758b24fd654e9
Peter Teuben <teuben@astro.umd.edu>
1999-10-26 19:48:26 +0000

    Initial revision

---
+++ rc/ds9.sh
@@ -0,0 +1,6 @@
+# no need to source anything, since it's in /usr/local/{bin,lib} etc.
+# this is just a placeholder to keep automatic loader scripts happy
+
+# pjt's version is in /usr/local/ds9 ; also had mirds9 and bswap
+
+export PATH=${PATH}:/usr/local/ds9
