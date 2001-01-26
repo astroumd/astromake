7ef14ae09db76fe41dca7f0b681448509b694fc0
Peter Teuben <teuben@astro.umd.edu>
2001-01-26 22:56:40 +0000

    astromake V0.98 important

---
+++ rc/xgobi.csh
@@ -0,0 +1,4 @@
+setenv XGOBID /usr/local/xgobi/
+# alias xgobi $XGOBID/src/xgobi
+# alias xgvis $XGOBID/src/xgvis
+
---
+++ rc/xgobi.sh
@@ -0,0 +1,5 @@
+export XGOBID=/usr/local/xgobi/
+
+#alias xgobi=$XGOBID/src/xgobi
+#alias xgvis=$XGOBID/src/xgvis
+
