d195fbae55ac01d2f49457c1c904df153cae5d14
Peter Teuben <teuben@astro.umd.edu>
2009-07-13 23:29:28 +0000

    try

--- rc/nemo.sh
+++ rc/nemo.sh
@@ -1,2 +1,19 @@
-# 
-echo NEMO startup not supported in SH-type shells, use a CSH-compatible shell
+
+dir=$ASTROMAKE/opt/nemo
+
+#	look for a new one
+if [ -e $dir/nemo_start.sh ]; then
+  export NEMO=$dir
+elif [ -z $a_version ]; then
+  export NEMO=$dir/$a_version
+elif [ -e $dir/VERSIONS ]; then
+  export NEMO=$dir/`head -1 $dir/VERSIONS`
+elif [ -e $ASTROMAKE/status/nemo ]; then
+  export NEMO=$dir/`cat $ASTROMAKE/status/nemo`
+fi
+
+if [ -e $NEMO/nemo_start.sh ]; then
+  source $NEMO/nemo_start.sh
+fi
+
+
