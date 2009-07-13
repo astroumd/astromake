7eb5a43f8b05e47fc89a96756b92c4e72c3a03d9
Peter Teuben <teuben@astro.umd.edu>
2009-07-13 23:18:55 +0000

    fix

--- make/astroload.sh
+++ make/astroload.sh
@@ -4,6 +4,19 @@
 # for now only handle pacakge name as first argument
 pkg=$1
 
-if [ -d $ASTROMAKE/opt/$pkg ]; then
-  source $ASTROMAKE/rc/$pkg.sh
+set tst1=
+
+if [ -z $pkg ]; then
+  echo Empty args supported yet
+elif [ -d $ASTROMAKE/opt/$pkg ]; then
+  echo checking $ASTROMAKE/opt/$pkg/VERSIONS
+  if [ -e $ASTROMAKE/opt/$pkg/VERSIONS ]; then
+    echo VERSIONS file found for $pkg
+  else
+    echo $ASTROMAKE/rc/$pkg.sh
+    source $ASTROMAKE/rc/$pkg.sh
+  fi
+else
+  echo Failed to astroload $pkg 
 fi
+
