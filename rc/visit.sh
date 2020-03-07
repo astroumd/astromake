56466d0ce62649391fab02af8049fbc04ab0226d
Peter Teuben <teuben@gmail.com>
2020-03-07 11:54:35 -0500

    proper bash version

--- rc/visit.sh
+++ rc/visit.sh
@@ -1,15 +1,12 @@
 # Bash environment for Visit
-# an actual bash user is encouraged to test/improve this
-# BUT, I can't get astromake to work with bash anyway, so no idea
-# how right or wrong this is
 
-dir="/astromake/opt/visit/1.6/bin"
 
-if [ $PATH ]
-then
-    export PATH="$dir/:$PATH"
-else
-    export PATH="$dir/"
-fi
+dir="$ASTROMAKE/opt/visit/"
+version=`cat $ASTROMAKE/status/visit`
 
-exit 0
+a_root=$dir/$version
+echo Loading visit from $a_root
+
+export PATH=$a_root/bin:$PATH
+
+
